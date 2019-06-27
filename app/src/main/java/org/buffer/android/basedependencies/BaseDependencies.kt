0ef83a9f5c15b9c93dab9b1e9dd3327599611792
Joe Birch <hitherejoebirch@googlemail.com>
2019-06-27 11:28:38 +0100

    Initial commit

---
+++ app/src/main/java/org/buffer/android/basedependencies/BaseDependencies.kt
@@ -0,0 +1,9 @@
+package org.buffer.android.basedependencies
+
+object BaseVersions {
+    val kotlin = "1.2.71"
+}
+
+object BaseDeps {
+    val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jre8:${BaseVersions.kotlin}"
+}
